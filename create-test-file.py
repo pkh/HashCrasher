d510f37f2e1c9da3d7e49f342794c2bb2414589d
Patrick Hanlon <patrick@doejo.com>
2013-10-21 14:07:39 -0500

    create-test-file.py mods

--- create-test-file.py
+++ create-test-file.py
@@ -4,17 +4,9 @@
 #	create-test-file.py
 #	Created by pkh on 2013-10-21
 #
-#	Hash type options:
-#	
-#
-#
-#
-#
 
 import random
 import hashlib
-import sys
-import getopt
 
 NUM_ENTRIES = 0
 HASH_TYPE = 0
@@ -24,15 +16,27 @@ HASH_TYPE = 0
 print "Welcome to create-test-file.py"
 print "How many hashes would you like in your file?"
 NUM_ENTRIES = int(raw_input())
+print ""
 
 print "Please enter the corresponding code for the hash type you would like:"
 print "\t100 = MD5\n\t200 = SHA1\n\t300 = SHA224\n\t400 = SHA256\n\t500 = SHA384\n\t600 = SHA512"
 HASH_TYPE = int(raw_input())
-
 print ""
 
-
-print "Creating file with " + str(NUM_ENTRIES) + " items."
+hash_title = ""
+if HASH_TYPE == 100:
+	hash_title = 'MD5'
+elif HASH_TYPE == 200:
+	hash_title = 'SHA1'
+elif HASH_TYPE == 300:
+	hash_title = 'SHA224'
+elif HASH_TYPE == 400:
+	hash_title = 'SHA256'
+elif HASH_TYPE == 500:
+	hash_title = 'SHA384'
+elif HASH_TYPE == 600:
+	hash_title = 'SHA512'
+print "Creating file with " + str(NUM_ENTRIES) + " " + hash_title + " hashes."
 
 numbers = []
 hashes = []
@@ -54,11 +58,6 @@ for x in range(NUM_ENTRIES):
 
 	# print numlstString
 	numbers.append(numlstString)
-
-	# if lengthen == True:
-	# 	numlst.append(" *")
-
-	# print ''.join(numlst)
 
 
 
